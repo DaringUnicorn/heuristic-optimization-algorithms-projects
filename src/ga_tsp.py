c40616791e7837b4795edc7ca7653b3732c64b95
DaringUnicorn <ycihan24@gmail.com>
2025-05-31 10:49:10 +0300

    added the report

--- src/ga_tsp.py
+++ src/ga_tsp.py
@@ -1,50 +1,54 @@
 # src/ga_tsp.py
 
 import random
+import math
 from src.fitness import fitness
 from src.init_population import initialize_population
 
-def tournament_selection(pop, fits, k):
-    picks = random.sample(list(zip(pop, fits)), k)
-    return min(picks, key=lambda x: x[1])[0].copy()
+def two_opt(route, dist):
+    """
+    Basit 2-opt hill climbing: iyileşme kalmayana dek tüm i<j çifti dener.
+    """
+    n = len(route)
+    improved = True
+    while improved:
+        improved = False
+        for i in range(n - 1):
+            for j in range(i + 2, n):
+                # son eleman ile ilk arasında da 2-opt
+                if i == 0 and j == n-1:
+                    continue
+                # mevcut mesafe
+                a, b = route[i], route[i+1]
+                c, d = route[j], route[(j+1) % n]
+                before = dist[a][b] + dist[c][d]
+                after  = dist[a][c] + dist[b][d]
+                if after + 1e-9 < before:
+                    # inversion
+                    route[i+1:j+1] = reversed(route[i+1:j+1])
+                    improved = True
+        # döngü sonunda tekrar dene
+    return route
 
 def pmx_crossover(p1, p2):
-    """
-    Robust PMX for permutations p1, p2.
-    Returns two valid children c1, c2.
-    """
     n = len(p1)
-    # Child’ları None ile başlat
-    c1 = [None] * n
-    c2 = [None] * n
-
-    # 1) İki kesme noktası seç
-    i, j = sorted(random.sample(range(n), 2))
-
-    # 2) Segmenti takasla ve mapping dict oluştur
-    map1 = {}
-    map2 = {}
+    c1 = [None]*n; c2 = [None]*n
+    i, j = sorted(random.sample(range(n),2))
+    map1 = {}; map2 = {}
     for k in range(i, j+1):
         c1[k] = p2[k]
         c2[k] = p1[k]
         map1[p2[k]] = p1[k]
         map2[p1[k]] = p2[k]
-
-    # 3) Geriye kalan pozisyonları doldur
-    for k in list(range(0, i)) + list(range(j+1, n)):
-        # child1 için
+    for k in list(range(0,i))+list(range(j+1,n)):
         gene = p1[k]
-        # eğer gene map1 içinde, mapping zincirini takip et
         while gene in map1:
             gene = map1[gene]
         c1[k] = gene
-
-        # child2 için aynı mantık
         gene = p2[k]
         while gene in map2:
             gene = map2[gene]
         c2[k] = gene
-
     return c1, c2
 
 def swap_mutation(ind, mr):
@@ -62,30 +66,42 @@ def run_ga(dist_matrix,
            p_crossover=0.8,
            p_mutation=0.2,
            elite_size=2):
+    """
+    p_mutation: başlangıç mutasyon oranı (adaptive inner loop)
+    """
     n = len(dist_matrix)
     pop = initialize_population(pop_size, n)
-    best_ind, best_fit = None, float('inf')
+    best_ind, best_fit = None, math.inf
 
     for g in range(generations):
+        # ** Adaptive mutation rate **
+        mr = p_mutation * (1 - g/(generations-1))
+        
         fits = [fitness(ind, dist_matrix) for ind in pop]
         # elitizm
         paired = sorted(zip(fits, pop), key=lambda x: x[0])
         new_pop = [ind.copy() for _, ind in paired[:elite_size]]
 
-        # geri kalan
+        # yeni popülasyon
         while len(new_pop) < pop_size:
-            p1 = tournament_selection(pop, fits, tourn_size)
-            p2 = tournament_selection(pop, fits, tourn_size)
+            # tournament
+            p1 = min(random.sample(list(zip(pop,fits)), tourn_size), key=lambda x: x[1])[0].copy()
+            p2 = min(random.sample(list(zip(pop,fits)), tourn_size), key=lambda x: x[1])[0].copy()
+            # crossover
             if random.random() < p_crossover:
                 c1, c2 = pmx_crossover(p1, p2)
             else:
                 c1, c2 = p1.copy(), p2.copy()
-            new_pop.append(swap_mutation(c1, p_mutation))
+            # mutation + 2-opt local search
+            c1 = two_opt(swap_mutation(c1, mr), dist_matrix)
             if len(new_pop) < pop_size:
-                new_pop.append(swap_mutation(c2, p_mutation))
+                c2 = two_opt(swap_mutation(c2, mr), dist_matrix)
+            new_pop.append(c1)
+            if len(new_pop) < pop_size:
+                new_pop.append(c2)
 
         pop = new_pop
-        # güncel en iyi
+        # en iyiyi güncelle
         gen_best_fit, gen_best_ind = min((fitness(i, dist_matrix), i) for i in pop)
         if gen_best_fit < best_fit:
             best_fit, best_ind = gen_best_fit, gen_best_ind.copy()
--- test_ga.py
+++ test_ga.py
@@ -1,18 +1,25 @@
 # test_ga.py
 
+import argparse
 from src.parse_tsp import load_custom_tsp, compute_dist_matrix
 from src.ga_tsp import run_ga
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--pop_size', type=int, default=50)
+parser.add_argument('--generations', type=int, default=100)
+parser.add_argument('--mutation_rate', type=float, default=0.1)
+args = parser.parse_args()
 
 coords = load_custom_tsp("instances/tsp_100_1")
 D = compute_dist_matrix(coords)
 
 best, length = run_ga(
     D,
-    pop_size=25,
-    generations=50,
+    pop_size=args.pop_size,
+    generations=args.generations,
     tourn_size=5,
     p_crossover=0.8,
-    p_mutation=0.1,
+    p_mutation=args.mutation_rate,
     elite_size=2
 )
 
